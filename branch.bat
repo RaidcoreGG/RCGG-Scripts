29b500013bac070f438482ede46f4e124ea58a76
Delta <46466697+DeltaGW2@users.noreply.github.com>
2024-03-27 17:48:41 +0100

    Update branch.bat

--- branch.bat
+++ branch.bat
@@ -1,6 +1,6 @@
 git branch --show-current >> temp_file.h
 set /p branchname=<temp_file.h
 set branchmacro=#define BRANCH_NAME "%branchname%"
-echo #pragma once > Branch.h
-echo %branchmacro% >> Branch.h
+echo #pragma once > src/Branch.h
+echo %branchmacro% >> src/Branch.h
 del temp_file.h
