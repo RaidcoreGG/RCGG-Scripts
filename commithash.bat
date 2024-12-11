107a5928afe9a0d2b54e1f60ba9a5dff8c2cd5f7
Delta <46466697+DeltaGW2@users.noreply.github.com>
2024-12-11 21:02:13 +0100

    cleanup

--- commithash.bat
+++ commithash.bat
@@ -1,10 +1,10 @@
 git rev-parse HEAD > temp_file.h
-set /p commithash=<temp_file.h
-set commitmacro=#define COMMIT_HASH "%commithash%"
-echo #pragma once > CommitHash.h
-echo %commitmacro% >> CommitHash.h
+SET /p commithash=<temp_file.h
+SET commitmacro=#define COMMIT_HASH "%commithash%"
+ECHO #pragma once > CommitHash.h
+ECHO %commitmacro% >> CommitHash.h
 git rev-parse --short HEAD > temp_file.h
-set /p commithash_short=<temp_file.h
-set commitmacro_short=#define COMMIT_HASH_SHORT "%commithash_short%"
-echo %commitmacro_short% >> CommitHash.h
-del temp_file.h
+SET /p commithash_short=<temp_file.h
+SET commitmacro_short=#define COMMIT_HASH_SHORT "%commithash_short%"
+ECHO %commitmacro_short% >> CommitHash.h
+DEL temp_file.h
--- version.bat
+++ version.bat
@@ -1,18 +1,18 @@
 WMIC.EXE Alias /? >NUL 2>&1 || GOTO s_error
 
 FOR /F "skip=1 tokens=1-6" %%G IN ('WMIC Path win32_utctime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table') DO (
-   IF "%%~L"=="" goto s_done
-      Set _yyyy=%%L
-      Set _mm=%%J
-      Set _dd=%%G
-      Set _hour=%%H
+   IF "%%~L"=="" GOTO s_done
+      SET _yyyy=%%L
+      SET _mm=%%J
+      SET _dd=%%G
+      SET _hour=%%H
       SET _minute=%%I
 )
 :s_done
 
-echo #pragma once > src/Version.h
-echo #define V_MAJOR %_yyyy% >> src/Version.h
-echo #define V_MINOR %_mm% >> src/Version.h
-echo #define V_BUILD %_dd% >> src/Version.h
+ECHO #pragma once > src/Version.h
+ECHO #define V_MAJOR %_yyyy% >> src/Version.h
+ECHO #define V_MINOR %_mm% >> src/Version.h
+ECHO #define V_BUILD %_dd% >> src/Version.h
 SET /A var_res = %_hour% * 60 + %_minute%
-echo #define V_REVISION %var_res% >> src/Version.h
+ECHO #define V_REVISION %var_res% >> src/Version.h
