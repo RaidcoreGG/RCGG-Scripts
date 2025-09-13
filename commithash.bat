a35b08542a658720a16c55ab906c94670082bef1
Delta <46466697+DeltaGW2@users.noreply.github.com>
2025-09-13 21:34:55 +0200

    Update scripts.

--- commithash.bat
+++ commithash.bat
@@ -1,10 +1,11 @@
 git rev-parse HEAD > temp_file.h
 SET /p commithash=<temp_file.h
 SET commitmacro=#define COMMIT_HASH "%commithash%"
-ECHO #pragma once > CommitHash.h
-ECHO %commitmacro% >> CommitHash.h
+ECHO #pragma once > src/CommitHash.h
+ECHO %commitmacro% >> src/CommitHash.h
+
 git rev-parse --short HEAD > temp_file.h
 SET /p commithash_short=<temp_file.h
 SET commitmacro_short=#define COMMIT_HASH_SHORT "%commithash_short%"
-ECHO %commitmacro_short% >> CommitHash.h
+ECHO %commitmacro_short% >> src/CommitHash.h
 DEL temp_file.h
